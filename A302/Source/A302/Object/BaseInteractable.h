5dec25c053c2300fcace09d48189201cdcc3f378
ssafyswh <ssafyswh@gmail.com>
2026-02-26 11:25:28 +0900

    feat: 상호 작용 가능한 오브젝트 예시 기초 작성

---
+++ A302/Source/A302/Object/BaseInteractable.h
@@ -0,0 +1,26 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Interface/InteractableInterface.h"
+#include "BaseInteractable.generated.h"
+
+UCLASS()
+class A302_API ABaseInteractable : public AActor, public IInteractableInterface
+{
+	GENERATED_BODY()
+	
+public:	
+	ABaseInteractable();
+
+protected:
+	// 상호작용 시 실행될 로직 (인터페이스 함수 오버라이드)
+	virtual void Interact(AActor* Interactor) override;
+    
+	// 조준 시 띄울 텍스트 (인터페이스 함수 오버라이드)
+	virtual FString GetInteractText() const override;
+
+	// 시각적 확인을 위한 컴포넌트
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+	TObjectPtr<UStaticMeshComponent> Mesh;
+};
