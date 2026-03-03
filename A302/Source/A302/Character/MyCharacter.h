96dc2c743d741aed3662655b2d34865a372aaaed
ssafyswh <ssafyswh@gmail.com>
2026-03-03 15:00:35 +0900

    refactor: MyCharacter.h

--- A302/Source/A302/Character/MyCharacter.h
+++ A302/Source/A302/Character/MyCharacter.h
@@ -19,35 +19,55 @@ class ADummyCharacter;
 UCLASS()
 class A302_API AMyCharacter : public ACharacter
 {
-	GENERATED_BODY()
+    GENERATED_BODY()
 
 public:
-	// Sets default values for this character's properties
-	AMyCharacter();
+    // Sets default values for this character's properties
+    AMyCharacter();
+
+    // Called every frame
+    virtual void Tick(float DeltaTime) override;
+
+    // Called to bind functionality to input
+    virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
 protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
+    // Called when the game starts or when spawned
+    virtual void BeginPlay() override;
 
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
+    // Input Actions
+    UPROPERTY(EditDefaultsOnly, Category = "Input")
+    UInputAction* IA_Move;
 
-	// Called to bind functionality to input
-	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+    UPROPERTY(EditDefaultsOnly, Category = "Input")
+    UInputAction* IA_Look;
 
-protected:
-	UPROPERTY(EditDefaultsOnly, Category = "Input")
-	UInputAction* IA_Move;
+    UPROPERTY(EditDefaultsOnly, Category = "Input")
+    UInputAction* IA_Jump;
+    
+    UPROPERTY(EditDefaultsOnly, Category = "Input")
+    UInputAction* IA_Interact;
 
-	UPROPERTY(EditDefaultsOnly, Category = "Input")
-	UInputAction* IA_Look;
+    // 상호작용 가능 거리를 설정합니다.
+    UPROPERTY(EditAnywhere, Category = "Interaction")
+    float InteractionDistance = 300.f;
 
-	UPROPERTY(EditDefaultsOnly, Category = "Input")
-	UInputAction* IA_Jump;
-	
-	UPROPERTY(EditDefaultsOnly, Category = "Input")
-	UInputAction* IA_Interact;
+    // 매 프레임 상호작용 대상을 체크하는 함수
+    void CheckForInteractables();
+    
+    void ToggleHighlight(AActor* TargetActor, bool bIsOn);
+
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "UI")
+    TSubclassOf<UUserWidget> InteractionWidgetClass;
+    
+    UPROPERTY()
+    UUserWidget* InteractionWidgetInstance;
+    
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "UI")
+    TSubclassOf<UUserWidget> CrosshairWidgetClass;
+    
+    UPROPERTY()
+    UUserWidget* CrosshairWidgetInstance;
 
     UPROPERTY(EditDefaultsOnly, Category = "Item|Definition")
     TObjectPtr<UItemDefinition> KnifeDef;
@@ -67,40 +87,19 @@ protected:
     UPROPERTY(EditAnywhere, Category = "Item|Test", meta=(ClampMin="0.0"))
     float AutoWarpDistanceToDummy = 120.f;
 
-private:
-	void OnMove(const FInputActionValue& Value);
-	void OnLook(const FInputActionValue& Value);
-	void OnJump(const FInputActionValue& Value);
-	void OnJumpReleased(const FInputActionValue& Value);
-	void OnInteract(const FInputActionValue& Value);
-	
-	UPROPERTY()
-	AActor* LastInteractableActor = nullptr;
-	void ToggleHighlight(AActor* TargetActor, bool bIsOn);
-	
-protected:
-	// 상호작용 가능 거리를 설정합니다.
-	UPROPERTY(EditAnywhere, Category = "Interaction")
-	float InteractionDistance = 300.f;
-
-	// 매 프레임 상호작용 대상을 체크하는 함수
-	void CheckForInteractables();
-	
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "UI")
-	TSubclassOf<UUserWidget> InteractionWidgetClass;
-	
-	UPROPERTY()
-	UUserWidget* InteractionWidgetInstance;
-	
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "UI")
-	TSubclassOf<UUserWidget> CrosshairWidgetClass;
-	
-	UPROPERTY()
-	UUserWidget* CrosshairWidgetInstance;
-
     void SetupKnifeForTest();
     void FindAndWarpNearDummy();
     void ExecuteAutoKnifeAttack();
+
+private:
+    void OnMove(const FInputActionValue& Value);
+    void OnLook(const FInputActionValue& Value);
+    void OnJump(const FInputActionValue& Value);
+    void OnJumpReleased(const FInputActionValue& Value);
+    void OnInteract(const FInputActionValue& Value);
+
+    UPROPERTY()
+    AActor* LastInteractableActor = nullptr;
 
     UPROPERTY()
     TObjectPtr<UItemActionFactory> ItemActionFactory;
