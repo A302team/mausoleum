918a3cdd9991067bc9bf01b5df51b3a219d8aead
unknown <sikk806@gmail.com>
2026-03-04 14:29:20 +0900

    ✨ Feat: A302 GameMode 추가 [S14P21A302-51]

--- A302/Source/A302/GameMode/A302GameMode.cpp
+++ A302/Source/A302/GameMode/A302GameMode.cpp
@@ -6,10 +6,12 @@
 #include "GameMode/A302PlayerState.h"
 #include "Character/MyCharacter.h"
 #include "Character/MyPlayerController.h"
+#include "Server/SpawnManager.h"
+#include "Kismet/GameplayStatics.h"
 
 AA302GameMode::AA302GameMode()
 {
-    DefaultPawnClass        = AMyCharacter::StaticClass();
+    DefaultPawnClass        = nullptr;
     PlayerControllerClass   = AMyPlayerController::StaticClass();
     GameStateClass          = AA302GameState::StaticClass();
     PlayerStateClass        = AA302PlayerState::StaticClass();
@@ -18,17 +20,60 @@ AA302GameMode::AA302GameMode()
 void AA302GameMode::BeginPlay()
 {
     Super::BeginPlay();
-    UE_LOG(LogTemp, Log, TEXT("[A302GameMode] BeginPlay"));
+
+    TArray<AActor*> FoundActors;
+    UGameplayStatics::GetAllActorsOfClass(GetWorld(), ASpawnManager::StaticClass(), FoundActors);
+
+    if(FoundActors.Num() > 0)
+    {
+        SpawnManager = Cast<ASpawnManager>(FoundActors[0]);
+        UE_LOG(LogTemp, Log, TEXT("[GameMode/A302GameMode] Find SpawnManager."));
+    }
+    else
+    {
+        UE_LOG(LogTemp, Log, TEXT("[GameMode/A302GameMode] Can't find SpawnManager"));
+    }
+
+    UE_LOG(LogTemp, Log, TEXT("[GameMode/A302GameMode] BeginPlay"));
 }
 
 void AA302GameMode::PostLogin(APlayerController* NewPlayer)
 {
     Super::PostLogin(NewPlayer);
-    UE_LOG(LogTemp, Log, TEXT("[A302GameMode] 플레이어 접속"));
+
+    FInputModeGameOnly InputMode;
+    NewPlayer->SetInputMode(InputMode);
+    NewPlayer->bShowMouseCursor = false;
+
+    UE_LOG(LogTemp, Log, TEXT("[GameMode/A302GameMode] 플레이어 접속"));
+
+    SpawnPlayer(NewPlayer);
 }
 
 void AA302GameMode::Logout(AController* Exiting)
 {
     Super::Logout(Exiting);
-    UE_LOG(LogTemp, Log, TEXT("[A302GameMode] 플레이어 퇴장"));
+    UE_LOG(LogTemp, Log, TEXT("[GameMode/A302GameMode] 플레이어 퇴장"));
+}
+
+void AA302GameMode::SpawnPlayer(APlayerController* PlayerController)
+{
+    if(!SpawnManager)
+    {
+        UE_LOG(LogTemp, Warning, TEXT("[GameMode/A302GameMode] No SpawnManager."));
+        return;
+    }
+
+    FTransform SpawnTransform = SpawnManager->GetRandomPlayerSpawnTransform(CurrentStage);
+
+    AMyCharacter* Character = GetWorld()->SpawnActor<AMyCharacter>(
+        AMyCharacter::StaticClass(),
+        SpawnTransform
+    );
+
+    if(Character)
+    {
+        PlayerController->Possess(Character);
+        UE_LOG(LogTemp, Log, TEXT("[GameMode/A302GameMode] Success SpawnPlayer."));
+    }
 }
