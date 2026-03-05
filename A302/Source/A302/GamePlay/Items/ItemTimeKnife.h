082c90b193057ba71237c21180d01fa447baa5b9
kke3436 <kke3436@naver.com>
2026-03-05 15:31:03 +0900

    ✨ Feat: TimerKnife 만듦 [S14P21A302-48]

---
+++ A302/Source/A302/GamePlay/Items/ItemTimeKnife.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GamePlay/Items/ItemKnife.h"
+#include "ItemTimeKnife.generated.h"
+
+UCLASS(BlueprintType)
+class A302_API UItemTimeKnife : public UItemKnife
+{
+	GENERATED_BODY()
+};
+
