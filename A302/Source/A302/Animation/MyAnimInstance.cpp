be6804074dad4f76b665dc46c3cd181413320de8
means-kim <kms99400@gmail.com>
2026-03-07 18:14:48 +0900

    ✨ Feat: 방어 애니메이션과 방패 시스템 연동 [S14P21A302-69]

--- A302/Source/A302/Animation/MyAnimInstance.cpp
+++ A302/Source/A302/Animation/MyAnimInstance.cpp
@@ -1,13 +1,26 @@
 #include "Animation/MyAnimInstance.h"
 #include "Character/MyCharacter.h"
+#include "Character/Components/CombatStatusComponent.h"
 #include "GameFramework/CharacterMovementComponent.h"
 
 void UMyAnimInstance::NativeInitializeAnimation()
 {
     Super::NativeInitializeAnimation();
+
     CachedCharacter = Cast<AMyCharacter>(TryGetPawnOwner());
+
+    if (CachedCharacter)
+    {
+        CachedCombatComponent = CachedCharacter->FindComponentByClass<UCombatStatusComponent>();
+
+        if (CachedCombatComponent)
+        {
+            PreviousShieldCount = CachedCombatComponent->ShieldBlockCount;
+        }
+    }
 }
 
+// 애니메이션 업데이트 함수 (매 프레임 호출)
 void UMyAnimInstance::NativeUpdateAnimation(float DeltaSeconds)
 {
     Super::NativeUpdateAnimation(DeltaSeconds);
@@ -26,9 +39,6 @@ void UMyAnimInstance::NativeUpdateAnimation(float DeltaSeconds)
 
     bIsInAir = CachedCharacter->GetCharacterMovement()->IsFalling();
 
-    FRotator BaseRotation = CachedCharacter->GetActorRotation();
-
-
     FVector Forward = CachedCharacter->GetActorForwardVector();
     FVector Right = CachedCharacter->GetActorRightVector();
 
@@ -40,5 +50,30 @@ void UMyAnimInstance::NativeUpdateAnimation(float DeltaSeconds)
     MoveDirection = FMath::RadiansToDegrees(FMath::Atan2(RightDot, ForwardDot));
     MoveDirection = FMath::Clamp(MoveDirection, -45.f, 45.f);
 
-    //bIsAttacking = CachedCharacter->bIsAttacking;
+    // Detect Shield Use → Play Block Animation
+    if (CachedCombatComponent)
+    {
+        int32 CurrentShieldCount = CachedCombatComponent->ShieldBlockCount;
+
+        if (CurrentShieldCount < PreviousShieldCount)
+        {
+            PlayBlockMontage();
+        }
+
+        PreviousShieldCount = CurrentShieldCount;
+    }
+}
+
+// 블록 몽타주 재생 함수 (방패 사용 시 호출)
+void UMyAnimInstance::PlayBlockMontage()
+{
+    if (!BlockMontage)
+    {
+        return;
+    }
+
+    if (!Montage_IsPlaying(BlockMontage))
+    {
+        Montage_Play(BlockMontage);
+    }
 }
