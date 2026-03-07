74ed72650a1fe30bf2ced44b21838c64bfdc1c6c
means-kim <kms99400@gmail.com>
2026-03-07 18:14:48 +0900

    ✨ Feat: 방어 애니메이션과 방패 시스템 연동 [S14P21A302-69]

--- A302/Source/A302/Animation/MyAnimInstance.h
+++ A302/Source/A302/Animation/MyAnimInstance.h
@@ -5,6 +5,8 @@
 #include "MyAnimInstance.generated.h"
 
 class AMyCharacter;
+class UCombatStatusComponent;
+class UAnimMontage;
 
 UCLASS()
 class A302_API UMyAnimInstance : public UAnimInstance
@@ -17,21 +19,37 @@ public:
 
 public:
 
+    // Movement
     UPROPERTY(BlueprintReadOnly, Category="Movement")
     float Speed = 0.f;
 
     UPROPERTY(BlueprintReadOnly, Category="Movement")
     bool bIsInAir = false;
-
-    UPROPERTY(BlueprintReadOnly, Category="Combat")
-    bool bIsAttacking = false;
-
+    
     UPROPERTY(BlueprintReadOnly, Category="Movement")
     float MoveDirection = 0.f;
 
+    // Combat
+    UPROPERTY(BlueprintReadOnly, Category="Combat")
+    bool bIsAttacking = false;
+
+    UPROPERTY(BlueprintReadOnly, Category="Combat")
+    bool bIsBlocking = false;
+
+    // Block Animations
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Combat")
+    UAnimMontage* BlockMontage;
+
+    UFUNCTION(BlueprintCallable)
+    void PlayBlockMontage();
 
 private:
 
     UPROPERTY()
     AMyCharacter* CachedCharacter = nullptr;
+
+    UPROPERTY()
+    UCombatStatusComponent* CachedCombatComponent = nullptr;
+
+    int32 PreviousShieldCount = 0;
 };
