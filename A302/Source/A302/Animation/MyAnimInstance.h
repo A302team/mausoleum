7ecb48c1b1407668c36156a057df1b6801ec38ac
means-kim <kms99400@gmail.com>
2026-03-08 20:15:33 +0900

    ✨ Feat: 공격 및 상호작용 인스턴스 코드 추가 [S14P21A302-69]

--- A302/Source/A302/Animation/MyAnimInstance.h
+++ A302/Source/A302/Animation/MyAnimInstance.h
@@ -14,34 +14,59 @@ class A302_API UMyAnimInstance : public UAnimInstance
     GENERATED_BODY()
 
 public:
+
     virtual void NativeInitializeAnimation() override;
     virtual void NativeUpdateAnimation(float DeltaSeconds) override;
 
 public:
 
     // Movement
+
     UPROPERTY(BlueprintReadOnly, Category="Movement")
     float Speed = 0.f;
 
     UPROPERTY(BlueprintReadOnly, Category="Movement")
     bool bIsInAir = false;
-    
+
     UPROPERTY(BlueprintReadOnly, Category="Movement")
     float MoveDirection = 0.f;
 
-    // Combat
+
+    // Combat State
+
     UPROPERTY(BlueprintReadOnly, Category="Combat")
     bool bIsAttacking = false;
 
     UPROPERTY(BlueprintReadOnly, Category="Combat")
     bool bIsBlocking = false;
 
-    // Block Animations
+    UPROPERTY(BlueprintReadOnly, Category="Interaction")
+    bool bIsInteracting = false;
+
+
+    // Montages
+
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Combat")
+    UAnimMontage* AttackMontage;
+
     UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Combat")
     UAnimMontage* BlockMontage;
 
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Interaction")
+    UAnimMontage* InteractMontage;
+
+
+    // Play Functions (AnimBP에서 호출)
+
+    UFUNCTION(BlueprintCallable)
+    void PlayAttackMontage();
+
     UFUNCTION(BlueprintCallable)
     void PlayBlockMontage();
+
+    UFUNCTION(BlueprintCallable)
+    void PlayInteractMontage();
+
 
 private:
 
