ec49bfcb452746012c7a923207f3b7e0c283e249
yoonpyo <cgantro@gmail.com>
2026-03-11 17:48:19 +0900

    🐛 Fix: 포트번호 변경 [S14P21A302-74]

--- Server/GameServer/src/main.cpp
+++ Server/GameServer/src/main.cpp
@@ -16,7 +16,7 @@ int main()
     // 보이스 서버 스레드 (포트 9100)
     std::thread voiceThread([]() {
         VoiceServer voiceServer;
-        voiceServer.run(40000);
+        voiceServer.run(48100);
     });
 
     lobbyThread.join();
