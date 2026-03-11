dec26eb5a9c0164b12d2f6f2b11378c5c608771a
yoonpyo <cgantro@gmail.com>
2026-03-11 10:28:44 +0900

    ♻️ Refactor: Code 내부 설명 추가 [S14P21A302-74]

--- Server/GameServer/src/common/IServer.h
+++ Server/GameServer/src/common/IServer.h
@@ -2,10 +2,16 @@
 #include "Platform.h"
 #include "logging/Logger.h"
 
+/**
+ * @brief 서버 인터페이스
+ * 
+ */
 class IServer {
 public:
     virtual ~IServer() = default;
     virtual void run(int port){
+
+        // Windows 소켓 초기화
 #ifdef _WIN32
         WSADATA wsaData;
         if(WSAStartup(MAKEWORD(2, 2), &wsaData) != 0){
@@ -13,7 +19,7 @@ public:
             return;
         }
 #endif
-
+        // 소켓 초기화
         if(!initSocket(port)){
 #ifdef _WIN32
             WSACleanup();
@@ -22,8 +28,8 @@ public:
         }
 
         LOG_INFO(tag(), "서버 시작! 포트:" << port);
-        onStarted(port);
-        runLoop();
+        onStarted(port); // 서버 시작 후 추가 초기화가 필요한 경우 오버라이드
+        runLoop(); // 서버 메인 루프
 
         CLOSE_SOCKET(sock);
 #ifdef _WIN32
@@ -36,7 +42,7 @@ protected:
     SocketType sock = INVALID_SOCK;
     bool running = true;
 
-    virtual const char* tag() const = 0;
+    virtual const char* tag() const = 0; // 로그 태그
     virtual bool initSocket(int port) = 0;
     virtual void onStarted(int port) {}
     virtual void runLoop() = 0;
--- Server/GameServer/src/common/ScopedTimer.h
+++ Server/GameServer/src/common/ScopedTimer.h
@@ -8,15 +8,16 @@
  * @brief RAII 스코프 프로파일러
  * @details
  * - 특정 코드 블록의 실행 시간을 측정하여 로그로 출력
- * - 사용 예시:
- *   LOG_INFO("MyTag", "This is an info message");
+ * - 사용 예시: `{ PROFILE_SCOPE("MyBlock"); 코드/// }`
  */
 class ScopedTimer
 {
 public:
     /**
      * @brief 생성자
-     * @param name 프로파일러 이름
+     * @param name 측정할 코드 블록의 이름 (로그에 출력됨)
+     * @details
+     * - 객체가 생성되는 시점의 시간을 기록
      */
     ScopedTimer(const std::string& name)
         : name(name)
@@ -41,7 +42,6 @@ public:
     }
 
 private:
-
     std::string name;
     std::chrono::high_resolution_clock::time_point start;
 };
