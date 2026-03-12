1e3aad50c632ac96524dcf03dc998a135f6f3729
kke3436 <kke3436@naver.com>
2026-03-12 16:31:56 +0900

    ✨ Feat: Base 구조에 코드 추가 [S14P21A302-84]

--- A302/Source/A302/GamePlay/Events/GroupEvents/BaseGroupEvent.h
+++ A302/Source/A302/GamePlay/Events/GroupEvents/BaseGroupEvent.h
@@ -5,6 +5,7 @@
 #include "BaseGroupEvent.generated.h"
 
 class AActor;
+class AMyPlayerController;
 class URewardDefinition;
 
 UCLASS(BlueprintType, Abstract)
@@ -14,6 +15,7 @@ class A302_API UBaseGroupEvent : public UBaseEvent
 
 public:
 	void InitializeContext(const URewardDefinition* InRewardDefinition, AActor* InSourceActor);
+	virtual bool SubmitVote(AMyPlayerController* VotingPlayerController, int32 TargetPlayerId) { return false; }
 
 protected:
 	const URewardDefinition* GetRewardDefinition() const { return RewardDefinition; }
