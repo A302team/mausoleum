ad1a2af6efb50af0f9df8ff7b7312cb5ee479768
kke3436 <kke3436@naver.com>
2026-03-12 16:57:23 +0900

    🐛 Fix: Inspect Malice Error [S14P21A302-84]

--- A302/Source/A302/Character/Components/MaliceComponent.h
+++ A302/Source/A302/Character/Components/MaliceComponent.h
@@ -2,6 +2,7 @@
 
 #include "CoreMinimal.h"
 #include "Components/ActorComponent.h"
+#include "Net/UnrealNetwork.h"
 #include "MaliceComponent.generated.h"
 
 DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnMaliceChanged, int32, NewCount);
@@ -13,8 +14,9 @@ class A302_API UMaliceComponent : public UActorComponent
 
 public:
 	UMaliceComponent();
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
 
-	UPROPERTY(BlueprintReadOnly, Category="Malice")
+	UPROPERTY(ReplicatedUsing = OnRep_MaliceCount, BlueprintReadOnly, Category="Malice")
 	int32 MaliceCount = 0;
 
 	UPROPERTY(BlueprintAssignable, Category="Malice")
@@ -25,4 +27,8 @@ public:
 
 	UFUNCTION(BlueprintCallable, Category="Malice")
 	void ConsumeMalice(int32 Count);
+
+private:
+	UFUNCTION()
+	void OnRep_MaliceCount();
 };
