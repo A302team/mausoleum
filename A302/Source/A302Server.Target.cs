c8012d7e4a7ca5b6aa4cf59fda48ebfafe844b0b
unknown <sikk806@gmail.com>
2026-03-12 17:25:30 +0900

    ✨ Feat: 게임 시작 시 데디케이트 서버 자동 접속 [S14P21A302-109]

--- A302/Source/A302Server.Target.cs
+++ A302/Source/A302Server.Target.cs
@@ -1,15 +1,12 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
 using UnrealBuildTool;
-using System.Collections.Generic;
 
 public class A302ServerTarget : TargetRules
 {
-	public A302ServerTarget( TargetInfo Target) : base(Target)
-	{
-		Type = TargetType.Server;
-		DefaultBuildSettings = BuildSettingsVersion.V6;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_7;
-		ExtraModuleNames.Add("A302");
-	}
+    public A302ServerTarget(TargetInfo Target) : base(Target)
+    {
+        Type = TargetType.Server;
+        DefaultBuildSettings = BuildSettingsVersion.Latest;
+        IncludeOrderVersion = EngineIncludeOrderVersion.Latest;
+        ExtraModuleNames.Add("A302");
+    }
 }
