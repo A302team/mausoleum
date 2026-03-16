3658b37178d09a62d40aa392a13edf86d4721337
swh4077 <swh4077@naver.com>
2026-03-16 15:44:56 +0900

    ✨ Feat: MaliceOverload 이벤트 개발

---
+++ A302/Source/A302/GamePlay/Events/PersonalEvents/PersonalEventMaliceOverload.h
@@ -0,0 +1,15 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GamePlay/Events/PersonalEvents/BasePersonalEvent.h"
+#include "PersonalEventMaliceOverload.generated.h"
+
+UCLASS()
+class A302_API UPersonalEventMaliceOverload : public UBasePersonalEvent
+{
+	GENERATED_BODY()
+
+public:
+	virtual void ExecuteEvent_Implementation(class AMyCharacter* InstigatorCharacter) override;
+	virtual void OnEventResolved_Implementation(class AMyCharacter* InstigatorCharacter, int32 ChoiceIndex) override;
+};
