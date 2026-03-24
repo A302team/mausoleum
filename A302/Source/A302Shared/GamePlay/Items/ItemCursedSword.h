287a11206f266f25c67b2e5f3783ea0dc6c1329c
yoonpyo <cgantro@gmail.com>
2026-03-24 13:21:47 +0900

    ✨ Feat: 저주검 타이머 및 아이템 효과 처리 개선 [S14P21A302-165]

--- A302/Source/A302Shared/GamePlay/Items/ItemCursedSword.h
+++ A302/Source/A302Shared/GamePlay/Items/ItemCursedSword.h
@@ -10,9 +10,9 @@ class A302SHARED_API UItemCursedSword : public UItemKnife
 	GENERATED_BODY()
 
 public:
+	virtual void OnItemAcquired(class ACharacter* OwnerCharacter) const override;
 	virtual bool Use_Implementation(ACharacter* Instigator, const FItemTargetData& TargetData) override;
 
 protected:
 	virtual void PlayUsePresentation(ACharacter* Instigator) override;
 };
-
