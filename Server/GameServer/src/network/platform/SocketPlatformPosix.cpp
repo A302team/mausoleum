80d352184bfe67ec1b3232651ac02f9e62ed0b00
yoonpyo <cgantro@gmail.com>
2026-03-26 09:24:32 +0900

    ♻️ Refactor: S14P21A302-194 SocketPlatform으로 소켓 라이프사이클 이관 [S14P21A302-17]

--- Server/GameServer/src/network/platform/SocketPlatformPosix.cpp
+++ Server/GameServer/src/network/platform/SocketPlatformPosix.cpp
@@ -1,5 +1,10 @@
 #ifndef _WIN32
 #include "network/platform/SocketPlatform.h"
+#if defined(__linux__)
+#include "network/platform/NetworkEventLoopLinux.h"
+#else
+#include "network/platform/INetworkEventLoop.h"
+#endif
 
 class PosixSocketPlatform final : public SocketPlatform {
 public:
@@ -8,6 +13,81 @@ public:
     }
 
     void shutdown() override {}
+
+    SocketType createUdpEndpoint(int port) override {
+        SocketType sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+        if (sock == INVALID_SOCK) {
+            return INVALID_SOCK;
+        }
+
+        configureUdpSocket(sock);
+
+        sockaddr_in addr{};
+        addr.sin_family = AF_INET;
+        addr.sin_addr.s_addr = INADDR_ANY;
+        addr.sin_port = htons(static_cast<uint16_t>(port));
+        if (bind(sock, reinterpret_cast<sockaddr*>(&addr), sizeof(addr)) == SOCK_ERROR) {
+            const int err = lastError();
+            closeSocket(sock);
+            errno = err;
+            return INVALID_SOCK;
+        }
+
+        if (!setNonBlocking(sock)) {
+            const int err = lastError();
+            closeSocket(sock);
+            errno = err;
+            return INVALID_SOCK;
+        }
+
+        return sock;
+    }
+
+    SocketType createTcpListener(int port) override {
+        SocketType sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+        if (sock == INVALID_SOCK) {
+            return INVALID_SOCK;
+        }
+
+        sockaddr_in addr{};
+        addr.sin_family = AF_INET;
+        addr.sin_addr.s_addr = INADDR_ANY;
+        addr.sin_port = htons(static_cast<uint16_t>(port));
+
+        int opt = 1;
+        setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+        if (bind(sock, reinterpret_cast<sockaddr*>(&addr), sizeof(addr)) == SOCK_ERROR) {
+            const int err = lastError();
+            closeSocket(sock);
+            errno = err;
+            return INVALID_SOCK;
+        }
+
+        if (listen(sock, SOMAXCONN) == SOCK_ERROR) {
+            const int err = lastError();
+            closeSocket(sock);
+            errno = err;
+            return INVALID_SOCK;
+        }
+
+        if (!setNonBlocking(sock)) {
+            const int err = lastError();
+            closeSocket(sock);
+            errno = err;
+            return INVALID_SOCK;
+        }
+
+        return sock;
+    }
+
+    bool closeSocket(SocketType& sock) override {
+        if (sock == INVALID_SOCK) {
+            return true;
+        }
+        const int rc = CLOSE_SOCKET(sock);
+        sock = INVALID_SOCK;
+        return rc != SOCK_ERROR;
+    }
 
     bool setNonBlocking(SocketType sock) override {
         int flags = fcntl(sock, F_GETFL, 0);
@@ -26,4 +106,14 @@ SocketPlatform& GetSocketPlatform() {
     static PosixSocketPlatform instance;
     return instance;
 }
+
+#if defined(__linux__)
+std::unique_ptr<INetworkEventLoop> CreateNetworkEventLoop() {
+    return std::make_unique<LinuxNetworkEventLoop>();
+}
+#else
+std::unique_ptr<INetworkEventLoop> CreateNetworkEventLoop() {
+    return nullptr;
+}
+#endif
 #endif
